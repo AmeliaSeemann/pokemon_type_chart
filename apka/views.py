945112d127c958865d2004814b18e875fd45c370
AmeliaSeemann <as311019@student.polsl.pl>
2024-04-24 19:49:28 +0200

    getting users input and giving simple http response

--- apka/views.py
+++ apka/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render,redirect,HttpResponse
+from .models import TypeChart
 # Create your views here.
 def index(request):
     return redirect('/search')
 def search(request):
-    return render(request, 'search.html',{})
-def result(request):
-    context={}
-    return render(request, 'result.html',context)
+    if request.get_full_path()=='/search/':
+        return render(request, 'search.html',{})
+    else:
+        at = request.GET['attack']
+        de = request.GET['defense']
+        return result(request,at,de)
+def result(request,a,d):
+    stage_one = TypeChart.objects.get(name=a)
+    stage_two =getattr(stage_one,d)
+    return HttpResponse(a+d+stage_two)
+    #return render(request, 'result.html',{})
