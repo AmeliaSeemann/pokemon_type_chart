d3fe25d2686221cc37c47e27efac40c4448f26bb
AmeliaSeemann <as311019@student.polsl.pl>
2024-04-24 14:35:52 +0200

    paths and placeholders

--- apka/views.py
+++ apka/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search(request):
+    return render(request, 'search.html',{})
+def result(request):
+    context={}
+    return render(request, 'result.html',context)
