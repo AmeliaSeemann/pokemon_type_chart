9ecaf80c953dafba3809d86bbcbfe387df33afef
AmeliaSeemann <as311019@student.polsl.pl>
2024-04-24 14:24:15 +0200

    TypeChart model and migrations

--- apka/models.py
+++ apka/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class TypeChart(models.Model):
+    name = models.CharField(max_length=100)
+    normal = models.CharField(max_length=5)
+    fire = models.CharField(max_length=5)
+    water = models.CharField(max_length=5)
+    electric = models.CharField(max_length=5)
+    grass = models.CharField(max_length=5)
+    ice = models.CharField(max_length=5)
+    fighting = models.CharField(max_length=5)
+    poison = models.CharField(max_length=5)
+    ground = models.CharField(max_length=5)
+    flying = models.CharField(max_length=5)
+    psychic = models.CharField(max_length=5)
+    bug = models.CharField(max_length=5)
+    rock = models.CharField(max_length=5)
+    ghost = models.CharField(max_length=5)
+    dragon = models.CharField(max_length=5)
+    dark = models.CharField(max_length=5)
+    steel = models.CharField(max_length=5)
+    fairy = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
--- apka/views.py
+++ apka/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    return redirect('/search')
 def search(request):
     return render(request, 'search.html',{})
 def result(request):
