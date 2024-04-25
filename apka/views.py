8af05e20fe721cc5cf51711aa48095e3e6e7f68e
AmeliaSeemann <as311019@student.polsl.pl>
2024-04-25 16:47:54 +0200

    added colors and "go back" button

--- apka/views.py
+++ apka/views.py
@@ -11,22 +11,51 @@ def search(request):
         de = request.GET['defense']
         return redirect('/result/?attack={}&defense={}'.format(at,de))
 def result(request):
-    a = request.GET['attack']
-    d = request.GET['defense']
-    stage_one = TypeChart.objects.get(name=a)
-    stage_two =getattr(stage_one,d)
-    class Info:
-        def __init__(self,at,de,ef):
-            self.at=at
-            self.de=de
-            if ef=="2":
-                self.ef="It's super effective!"
-            elif ef=="0.5":
-                self.ef="It's not very effective..."
-            elif ef=="0":
-                self.ef="It has no effect..."
-            else:
-                self.ef="Nothing special here..."
-    obj = Info(a,d,stage_two)
-    context = {'obj':obj}
-    return render(request, 'result.html',context)
+    if request.method == 'POST':
+        return redirect('/search/')
+    else:
+        try:
+            a = request.GET['attack']
+            d = request.GET['defense']
+        except KeyError:
+            return redirect('/search/')
+        else:
+            stage_one = TypeChart.objects.get(name=a)
+            stage_two =getattr(stage_one,d)
+            color_names = {
+            "normal": '#A8A77A',
+            "fire": '#EE8130',
+            "water": '#6390F0',
+            "electric": '#F7D02C',
+            "grass": '#7AC74C',
+            "ice": '#96D9D6',
+            "fighting": '#C22E28',
+            "poison": '#A33EA1',
+            "ground": '#E2BF65',
+            "flying": '#A98FF3',
+            "psychic": '#F95587',
+            "bug": '#A6B91A',
+            'rock': '#B6A136',
+            'ghost': '#735797',
+            'dragon': '#6F35FC',
+            'dark': '#705746',
+            'steel': '#B7B7CE',
+            'fairy': '#D685AD'
+        }
+            class Info:
+                def __init__(self,at,de,ef,c1,c2):
+                    self.c1 = c1
+                    self.c2 = c2
+                    self.at=at
+                    self.de=de
+                    if ef=="2":
+                        self.ef="It's super effective!"
+                    elif ef=="0.5":
+                        self.ef="It's not very effective..."
+                    elif ef=="0":
+                        self.ef="It has no effect..."
+                    else:
+                        self.ef="Nothing special here..."
+            obj = Info(a,d,stage_two,color_names[a],color_names[d])
+            context = {'obj':obj}
+            return render(request, 'result.html',context)
